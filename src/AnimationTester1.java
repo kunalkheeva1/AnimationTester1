ef76eceaf466bb1f88e5743b5d3f21a22e4d9d27
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-10-15 23:12:56 -0700

    added other method and coded the main class and method for whole execution

--- src/AnimationTester1.java
+++ src/AnimationTester1.java
@@ -1,10 +1,85 @@
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public class AnimationTester1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        final int iconWidth = 400;
+        final int iconHeight = 100;
+        final int carWidth= 100;
+        final int delay = 100;
+
+
+
+        final MovableShape shape = new PlaneShape(0,0,carWidth);
+        final CarShape.ShapeIcon icon = new CarShape.ShapeIcon(shape, iconWidth, iconHeight);
+
+        final JLabel label = new JLabel(icon);
+        frame.setLayout(new FlowLayout());
+        frame.add(label);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        Timer t = new Timer(delay, new
+                ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        shape.translate(1,0);
+                        label.repaint();
+                    }
+                });
+        t.start();
+    }
+}
+
+class PlaneShape implements MovableShape{
+    private int x;
+    private int y;
+    private int width;
+
+    public PlaneShape(int x, int y, int width){
+        this.x = x;
+        this.y = y;
+        this.width= width;
+    }
+
+    public void translate(double dx, double dy){
+        x+=dx;
+        y+=dy;
+    }
+
+    public void draw(Graphics2D graphics2D){
+        Rectangle2D.Double body = new Rectangle2D.Double(x + width / 3, y + width / 3, width, width / 6);
+
+        Point2D.Double l1 = new Point2D.Double(x + width / 2, y + width / 3);
+        Point2D.Double l2 = new Point2D.Double(x + width / 2, y + width / 6);
+        Point2D.Double l3 = new Point2D.Double(x + width / 1, y + width / 3);
+
+        Line2D.Double back = new Line2D.Double(l1,l2);
+        Line2D.Double front = new Line2D.Double(l2, l3);
+
+        Point2D.Double f1 = new Point2D.Double(x + width / 3 + width, y + width/3);
+        Point2D.Double f2 = new Point2D.Double(x + width / 3 + width + width/6, y + width / 3 + width/12);
+        Point2D.Double f3 = new Point2D.Double(x + width / 3 + width, y + width / 3 + width/6);
+
+        Line2D.Double top = new Line2D.Double(f1, f2);
+        Line2D.Double bottom = new Line2D.Double(f2, f3);
+
+        graphics2D.draw(body);
+        graphics2D.draw(back);
+        graphics2D.draw(front);
+        graphics2D.draw(top);
+        graphics2D.draw(bottom);
+    }
+
 
 }
 
@@ -48,5 +123,30 @@ public class AnimationTester1 {
         graphics2D.draw(backGlass);
     }
 
+    static class ShapeIcon implements Icon {
+        private int width;
+        private int height;
+        private MovableShape shape;
+
+        public ShapeIcon(MovableShape shape, int width, int height){
+            this.width = width;
+            this.height = height;
+            this.shape = shape;
+        }
+
+        public int getIconWidth() {
+            return width;
+        }
+        public int getIconHeight() {
+            return height;
+        }
+
+        public void paintIcon(Component c, Graphics g, int x, int y){
+            Graphics2D g2 = (Graphics2D)g;
+            shape.draw(g2);
+        }
+
+
+    }
 
 }

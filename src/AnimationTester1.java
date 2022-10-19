694c9390cf61788b5f74d1a17b939f36e2137923
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-10-18 21:19:46 -0700

    perfectly added the multiple cars and all are movable

--- src/AnimationTester1.java
+++ src/AnimationTester1.java
@@ -18,14 +18,25 @@ public class AnimationTester1 {
 
 
         final MovableShape shape = new PlaneShape(0,0,carWidth);
+
         final CarShape.ShapeIcon icon = new CarShape.ShapeIcon(shape, iconWidth, iconHeight);
+        final CarShape.ShapeIcon icon2 = new CarShape.ShapeIcon(shape, iconWidth, iconHeight);
+        final CarShape.ShapeIcon icon3 = new CarShape.ShapeIcon(shape, iconWidth, iconHeight);
 
         final JLabel label = new JLabel(icon);
+        final JLabel label1 = new JLabel(icon2);
+        final JLabel label2 = new JLabel(icon3);
+
+
         frame.setLayout(new FlowLayout());
         frame.add(label);
+        frame.add(label1);
+        frame.add(label2);
+        frame.setSize(500,600);
+        frame.setResizable(false);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+
         frame.setVisible(true);
 
         Timer t = new Timer(delay, new
@@ -34,13 +45,15 @@ public class AnimationTester1 {
                     public void actionPerformed(ActionEvent e) {
                         shape.translate(1,0);
                         label.repaint();
+                        label1.repaint();
+                        label2.repaint();
                     }
                 });
         t.start();
     }
 }
 
-class PlaneShape implements MovableShape{
+class PlaneShape implements MovableShape {
     private int x;
     private int y;
     private int width;
@@ -57,27 +70,32 @@ class PlaneShape implements MovableShape{
     }
 
     public void draw(Graphics2D graphics2D){
-        Rectangle2D.Double body = new Rectangle2D.Double(x + width / 3, y + width / 3, width, width / 6);
+        Rectangle2D.Double body = new Rectangle2D.Double(x, y+ width/6, width-1, width/6);
+        Ellipse2D.Double frontTire = new Ellipse2D.Double(x+width/6, y+width/3, width/6, width/6);
+        Ellipse2D.Double rearTire = new Ellipse2D.Double(x+width*2/3, y+width/3, width/6, width/6);
 
-        Point2D.Double l1 = new Point2D.Double(x + width / 2, y + width / 3);
-        Point2D.Double l2 = new Point2D.Double(x + width / 2, y + width / 6);
-        Point2D.Double l3 = new Point2D.Double(x + width / 1, y + width / 3);
+        Point2D.Double r1 = new Point2D.Double(x+width/6,y+width/6);
+        Point2D.Double r2 = new Point2D.Double(x+width/3, y);
+        Point2D.Double r3 = new Point2D.Double(x+width/3,y);
+        Point2D.Double r4 = new Point2D.Double(x+width*5/6, y+width/6);
 
-        Line2D.Double back = new Line2D.Double(l1,l2);
-        Line2D.Double front = new Line2D.Double(l2, l3);
+        Line2D.Double frontGlass = new Line2D.Double(r1,r2);
+        Line2D.Double roof = new Line2D.Double(r2, r3);
+        Line2D.Double rearGlass = new Line2D.Double(r3,r4);
 
-        Point2D.Double f1 = new Point2D.Double(x + width / 3 + width, y + width/3);
-        Point2D.Double f2 = new Point2D.Double(x + width / 3 + width + width/6, y + width / 3 + width/12);
-        Point2D.Double f3 = new Point2D.Double(x + width / 3 + width, y + width / 3 + width/6);
 
-        Line2D.Double top = new Line2D.Double(f1, f2);
-        Line2D.Double bottom = new Line2D.Double(f2, f3);
+        //changing the colors of the tires
+        graphics2D.setColor(Color.GREEN);
+        graphics2D.fill(frontTire);
+        graphics2D.setColor(Color.BLUE);
+        graphics2D.fill(rearTire);
+        graphics2D.setColor(Color.RED);
+        graphics2D.fill(body);
+        graphics2D.draw(frontGlass);
+        graphics2D.draw(roof);
+        graphics2D.draw(rearGlass);
 
-        graphics2D.draw(body);
-        graphics2D.draw(back);
-        graphics2D.draw(front);
-        graphics2D.draw(top);
-        graphics2D.draw(bottom);
+        //second car
 
     }
 
